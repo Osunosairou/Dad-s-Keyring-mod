39a29a78252109c928ae4d68eeb9d02b674a2dc4
Osunosairou <theonduarte@gmail.com>
2021-11-23 20:39:45 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,15 @@ local kringMod = RegisterMod("Dad's Keyring", 1)
 local game = Game()
 local kringId = Isaac.GetItemIdByName("Dad's Keyring")
 local hasKRing = false
+local keys = {
+    strangeKey = TrinketType.TRINKET_STRANGE_KEY,
+    crystalKey = TrinketType.TRINKET_CRYSTAL_KEY,
+    blueKey = TrinketType.TRINKET_BLUE_KEY,
+	gildedKey = TrinketType.TRINKET_GILDED_KEY,
+	rustedKey = TrinketType.TRINKET_RUSTED_KEY,
+	storeKey = TrinketType.TRINKET_STORE_KEY
+
+}
 
 
 -- Checks if the player has the item
@@ -17,10 +26,17 @@ kringMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, kringMod.onPlayerInit)
 
 -- Every major effect happens here
 function kringMod:onUpdate(player)
+
+    -- spawns the item at the start of the run
     if game:GetFrameCount() == 1 then
         print(kringId)
         Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, kringId, Vector(320,300), Vector(0,0), nil)
     end
+
+    if player:HasCollectible(kringId) and not hasKRing then
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TRINKET, keys.storeKey, Vector(320,280), Vector(0,0), nil)
+    end
+
     updateKRing(player)
 end
 kringMod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, kringMod.onUpdate)
